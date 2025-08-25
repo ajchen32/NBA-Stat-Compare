0b4a7aa05f7df9b34c6bc1f213af5736212aab9b
ajchen32 <ajchen32@gmail.com>
2025-08-24 20:49:46 -0500

    Updated working player web scraper with wait time

--- csvtoSQL.py
+++ csvtoSQL.py
@@ -1,3 +1,12 @@
 import pandas as pd
 import mysql.connector
 
+csv = ""
+
+connect = mysql.connect(host = "localhost", user = "root", password = "password123", database = "NBAPlayer202425")
+
+df = pd.read_csv(csv)
+
+cursor = connect.cursor()
+
+
