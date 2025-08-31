47d94865cfaa854ee648e53ec64a2e2afc23ba95
ajchen32 <ajchen32@gmail.com>
2025-08-31 06:11:00 -0500

    Some improvement

--- train.py
+++ train.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 import numpy as np
 import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
+from torch.utils.data import TensorDataset, DataLoader
 
 data = np.load('NBA-Stat-Compare/trainandtest_nparrays.npz')
 train_x = data['train_x']
@@ -12,7 +13,20 @@ test_y = data['test_y']
 
 
 trainx_tensor = torch.tensor(train_x, dtype=torch.float32)
-trainy_tensor = torch.tensor(train_y, dtype=torch.float32).view(-1, 1)
+trainy_tensor = torch.tensor(train_y, dtype=torch.float32)
+
+testx_tensor = torch.tensor(test_x, dtype=torch.float32)
+testy_tensor = torch.tensor(test_y, dtype=torch.float32)
+
+print(trainx_tensor.shape)
+print(trainy_tensor.shape)
+
+train_dataset = TensorDataset(trainx_tensor, trainy_tensor)
+train_loader = DataLoader(train_dataset, batch_size = 16, shuffle = True)
+
+test_dataset = TensorDataset(testx_tensor, testy_tensor)
+test_loader = DataLoader(test_dataset, batch_size = 16)
+
 
 
 class LSTMModel(nn.Module):
@@ -28,16 +42,38 @@ class LSTMModel(nn.Module):
         out = self.fc(out)
         return out
     
-input_size = 
-hidden_layer = 
-output_size = 
-num_layers = 
+input_size = trainx_tensor.shape[2]
+hidden_layer = 64
+output_size = trainy_tensor.shape[1]
+num_layers = 1
 
 my_model = LSTMModel(input_size, hidden_layer, output_size, num_layers)
 loss_model = nn.MSELoss()
 optimizer = torch.optim.Adam(my_model.parameters(), lr = .001)
 
 epochs = 50
+for e in range(epochs):
+    for ep_x, ep_y in train_loader:
+        optimizer.zero_grad()
+        y_pred = my_model(ep_x)
+        loss = loss_model(y_pred, ep_y)
+        loss.backward()
+        optimizer.step()
+
+
+    my_model.eval()
+    with torch.no_grad():
+        test_loss = sum(loss_model(my_model(batch_x), batch_y) for batch_x, batch_y in test_loader) / len(test_loader)
+    
+    if (e+1) % 10 == 0:
+        print(f"Epoch {e+1}, Train Loss: {loss.item():.4f}, Test Loss: {test_loss.item():.4f}")
+
+
+
+
+
+
+
 
 # Define the LSTM Model
 # class BasketballLSTM(nn.Module):
